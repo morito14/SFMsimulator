d4d13fdc4f9d7b204b5a686a65b966c03f9f65a0
Morito Sato <huroppi.tsukuba@gmail.com>
2019-02-05 15:02:49 +0900

    add SFMSimulator.generate_pedestrian()

--- src/sfm_simulator.py
+++ src/sfm_simulator.py
@@ -16,14 +16,17 @@ class SFMSimulator(Drawer, object):
         zoom = kwargs['zoom'] if 'zoom' in kwargs else 1
         super(SFMSimulator, self).__init__(self.map, zoom)
         # initialize pedestrian
-        self.pedestrian = Pedestrian()
+        self.pedestrians = []
 
     def debug(self):
-        # print('map={0}'.format(self.map.get_map()))
-        self.pedestrian.set_status(speed=600, position=[10, 500])
-        print('pedestrian status:{0}'.format(self.pedestrian.get_status()))
-        print('map status:{0}'.format(self.map.get_status()))
-        print('map.height:{0}, map.width:{1}'.format(self.map.height, self.map.width))
+        for num in range(10):
+            self.generate_pedestrian(speed=num)
+        for pedestrian in self.pedestrians:
+            print(pedestrian.speed)
 
         while True:
-            self.draw(self.pedestrian)
+            self.draw(self.pedestrians)
+
+    def generate_pedestrian(self, **kwargs):
+        self.pedestrians.append(Pedestrian())
+        self.pedestrians[-1].set_status(**kwargs)
